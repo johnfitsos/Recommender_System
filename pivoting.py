6202330280229382eb220ce56ffb2cf80832f1a0
johnfitsos <73551927+johnfitsos@users.noreply.github.com>
2020-12-20 17:24:07 +0200

    Delete pivoting.py

--- pivoting.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import re
-
-df = pd.read_csv('Preprocessing_columns.csv', sep =';', encoding='Latin-1',names=["user-id","rating","book_title"])
-print(df)
-df.to_csv('prepivoting.csv') ## We add columns in the previous csv
-
